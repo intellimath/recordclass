e03d83d743721a21cc435d248b9bae796ef0f07e
Zaur Shibzukhov <szport@gmail.com>
2020-09-15 13:50:24 +0300

    Set version to 0.14

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ if _PY36:
 
 setup(
     name = 'recordclass',
-    version = '0.13.2',
+    version = '0.14.0',
     description = description,
     author = 'Zaur Shibzukhov',
     author_email = 'szport@gmail.com',
