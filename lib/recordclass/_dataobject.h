37f1543cab9dc8ab430fcd49bda203e9f9e85ecc
Zaur Shibzukhov <szport@gmail.com>
2021-05-21 12:30:01 +0300

    Strip datatuple macros from _dataobject.h

--- lib/recordclass/_dataobject.h
+++ lib/recordclass/_dataobject.h
@@ -1,17 +1,8 @@
-#define PyDataTuple_ITEMS(type, op) (PyObject**)((char*)(op) + type->tp_basicsize)
-
 #define PyDataObject_SLOTS(op) (PyObject**)((char*)(op) + sizeof(PyObject))
-#define PyDataTuple_SLOTS(op) (PyObject**)((char*)(op) + sizeof(PyVarObject))
-
-#define PyDataTuple_NUMSLOTS(tp) ((tp->tp_basicsize - sizeof(PyVarObject))/sizeof(PyObject*) - \
-                                 (tp->tp_dictoffset?1:0) - \
-                                 (tp->tp_weaklistoffset?1:0))
 
 #define PyDataObject_NUMSLOTS(tp) ((tp->tp_basicsize - sizeof(PyObject))/sizeof(PyObject*)) - \
                                   (tp->tp_dictoffset?1:0) - \
                                   (tp->tp_weaklistoffset?1:0)
-
-#define PyDataTuple_NUMITEMS(op) Py_SIZE(op)
 
 #define PyDataObject_DICTPTR(type, op) ((PyObject**)((char*)(op) + type->tp_dictoffset))
 #define PyDataObject_WEAKLISTPTR(type, op) ((PyObject**)((char*)op + type->tp_weaklistoffset))
