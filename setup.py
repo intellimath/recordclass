28ab56b103b1a26c62bd6ac39dd24f0a87702118
Zaur Shibzukhov <szport@gmail.com>
2022-01-27 23:25:26 +0300

    Add python 3.10 support

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
