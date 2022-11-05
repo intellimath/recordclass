f804e17cdf87c40d62160f5d163198406a682cfd
Zaur Shibzukhov <szport@gmail.com>
2022-11-05 19:49:15 +0300

    Move test_dataobject_match.py to recordclass.test.match subpackage

---
+++ lib/recordclass/test/match/__init__.py
@@ -0,0 +1,23 @@
+# {{LICENCE}}
+
+from recordclass.test.test_recordclass import *
+from recordclass.test.test_arrayclass import *
+from recordclass.test.test_dataobject import *
+from recordclass.test.test_litelist import *
+from recordclass.test.test_litetuple import *
+
+import sys
+_PY36 = sys.version_info[:2] >= (3, 6)
+_PY310 = sys.version_info[:2] >= (3, 10)
+
+if _PY36:
+    from recordclass.test.typing.test_recordclass import *
+    from recordclass.test.typing.test_dataobject import *
+    pass
+
+if _PY310:
+    from recordclass.test.test_dataobject_match import *
+
+def test_all():
+    import unittest
+    unittest.main(verbosity=3)
